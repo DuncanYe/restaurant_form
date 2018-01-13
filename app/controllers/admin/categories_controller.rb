423a637695623c7c92c253e28324c43db757d206
Duncan <loxa10077@gmail.com>
2018-01-13 23:30:01 +0800

    complete restaurant index page for public users

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,4 @@
-class Admin::CategoriesController < Admin::BaseControlle
+class Admin::CategoriesController < Admin::BaseController
   before_action :set_category, only: [:update, :destroy]
 
   def index
