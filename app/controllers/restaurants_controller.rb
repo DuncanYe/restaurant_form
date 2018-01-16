f2c665e1dcd3180cf8290e36202a9f87abdeab49
Duncan <loxa10077@gmail.com>
2018-01-16 21:43:36 +0800

    rails generate controller categories

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,12 +3,4 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
   end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
-  private
-
-
 end
