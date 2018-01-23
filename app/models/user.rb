8151af761253aa759985b7c29953bd3a59e8a511
Duncan <loxa10077@gmail.com>
2018-01-23 11:01:51 +0800

    create favorite model and setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :comments, dependent: :restrict_with_error
-
-  has_many :restaurants, through: :comments         
-
+  
   validates_presence_of :name
 
   mount_uploader :avatar, AvatarUploader
+  
+  # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+  has_many :comments, dependent: :restrict_with_error
+  # 「使用者評論很多餐廳」的多對多關聯
+  has_many :restaurants, through: :comments
 
+  # 「使用者加入我的最愛」的多對多關聯
+  # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :favorited_restaurants
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  
+  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
   end
